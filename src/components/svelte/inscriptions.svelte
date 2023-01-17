3efce2862c29e01a7c18d4550866153e57cbed77
Nico-L <nicolas@luchier.fr>
2023-01-17 13:05:19 +0000

    gestionbouton

--- src/components/svelte/inscriptions.svelte
+++ src/components/svelte/inscriptions.svelte
@@ -477,6 +477,20 @@ async function envoiMail(uuid) {
                         Enregistrer
                     </button>
                     {/if}
+                    {#if flagSaving}
+                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-current stroke-current text-bleuLBF-500 bg-bleuLBF-500 h-8 w-8 mx-auto mt-1" viewBox="0 0 50 50">
+                        <g fill="none" fill-rule="evenodd" stroke-width="2" stroke="currentColor">
+                            <circle cx="22" cy="22" r="1">
+                                <animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
+                                <animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
+                            </circle>
+                            <circle cx="22" cy="22" r="1">
+                                <animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
+                                <animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
+                            </circle>
+                        </g>
+                    </svg>
+                    {/if}
                 </div>
             {/if}
 		{/if}
