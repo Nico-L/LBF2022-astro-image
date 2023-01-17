4a0be1dbe22622aad2a80701d57ca105834f32d2
Nico-L <nicolas@luchier.fr>
2023-01-17 13:18:58 +0000

    button style

--- src/components/svelte/inscriptions.svelte
+++ src/components/svelte/inscriptions.svelte
@@ -137,6 +137,7 @@
     }
 
 async function insertInscrits() {
+    if (flagSaving) return
     flagSaving = true
     if (listeInscrits.length > 0) {
         await functionsCall("saveInscription", {inscriptions: JSON.stringify(listeInscrits)})
@@ -472,25 +473,27 @@ async function envoiMail(uuid) {
                             Se désinscrire
                         </button>
                     {/if}
-                    {#if flagSaveValide && !flagSaving}
-                    <button on:click={insertInscrits}  class="mt-1 mx-1 px-1 border-2 border-bleuLBF rounded text-base font-medium text-bleuLBF">
-                        Enregistrer
+                    {#if flagSaveValide}
+                    <button on:click={insertInscrits}  class="mt-1 mb-0 mx-1 px-1 h-8 w-24 border-2 border-bleuLBF rounded text-base font-medium text-bleuLBF">
+                        {#if flagSaving}
+                            <svg xmlns="http://www.w3.org/2000/svg" class="fill-current stroke-current text-bleuLBF-500 bg-bleuLBF-500 h-6 w-6 mx-auto" viewBox="0 0 50 50">
+                                <g fill="none" fill-rule="evenodd" stroke-width="2" stroke="currentColor">
+                                    <circle cx="22" cy="22" r="1">
+                                        <animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
+                                        <animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
+                                    </circle>
+                                    <circle cx="22" cy="22" r="1">
+                                        <animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
+                                        <animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
+                                    </circle>
+                                </g>
+                            </svg>
+                        {:else}
+                            <span>Enregistrer</span>
+                        {/if}
                     </button>
                     {/if}
-                    {#if flagSaving}
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-current stroke-current text-bleuLBF-500 bg-bleuLBF-500 h-8 w-8 mx-auto mt-1" viewBox="0 0 50 50">
-                        <g fill="none" fill-rule="evenodd" stroke-width="2" stroke="currentColor">
-                            <circle cx="22" cy="22" r="1">
-                                <animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
-                                <animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
-                            </circle>
-                            <circle cx="22" cy="22" r="1">
-                                <animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/>
-                                <animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/>
-                            </circle>
-                        </g>
-                    </svg>
-                    {/if}
+                    
                 </div>
             {/if}
 		{/if}
